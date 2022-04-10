4f23f2e8acd476eea2b4bf64805a14f1317c6350
priyabdatta <priyankabdatta@gmail.com>
2022-04-09 19:12:59 -0600

    Update

---
+++ src/test/java/testcases/DataProviderExample.java
@@ -0,0 +1,28 @@
+package testcases;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderExample {
+	
+	@Test(dataProvider="dp-name")
+	public void PrintData(String data) {
+		
+		System.out.println("The data is "+ data);
+		
+	}
+	
+	@DataProvider(name="dp-name")
+	
+	public Object[][] dpMethod(){
+		
+		return new Object[][] {
+			{"First-data"},
+			{"Second-data"},
+			{"Third-data"},
+			{"Fourth-data"}
+			
+		};
+	}
+
+}
--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import Pages.LoginPage;
+
+@Listeners(TestNgListenerClass.class)
 
 public class LoginTest extends BaseClass{
 	
